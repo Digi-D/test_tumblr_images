5b4a124b3db247d927b32d940454e6386b3534fb
Dima Strakovsky <shifting.planes@gmail.com>
2014-12-26 00:15:30 -0600

    basic app

---
+++ test_1_app.js
@@ -0,0 +1,25 @@
+var request = require("request");
+
+var url = "http://rad0m1mg.tumblr.com/api/read/json";
+
+request({
+    url: url,
+    json: true
+}, function (error, response, body) {
+
+    if (!error && response.statusCode === 200) {
+        //console.log(body) // Print the json response
+        var inhale_jsonp=body;
+
+        //clean and parse JSONp response from tumblr API
+        var blog_stream= inhale_jsonp.substring(22,inhale_jsonp.length-2);
+        blog_stream=JSON.parse(blog_stream);
+
+        //console.log(blog_stream['posts'][0]['photo-url-500']);
+
+        for(var i=0;i<blog_stream.posts.length;i++)
+        {
+          console.log(i+": "+ blog_stream['posts'][i]['photo-url-500']);
+        }
+    }
+});
