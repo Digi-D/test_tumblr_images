979bdc7e50cdcb17755f3e7cb6b553631daa7a95
Dima Strakovsky <shifting.planes@gmail.com>
2015-01-05 23:54:29 -0500

    every call to root dir images are updated

--- test_1_app.js
+++ test_1_app.js
@@ -1,5 +1,3 @@
-// on heroku at https://floating-retreat-1732.herokuapp.com
-
 var express = require("express");
 var request = require("request");
 var hbs = require("hbs");
@@ -14,43 +12,39 @@ app.set('view engine', 'html');
 app.engine('html', require('hbs').__express);
 
 
-//var url = "http://rad0m1mg.tumblr.com/api/read/json";
 
+function GetPhotoURLs(url,res){
 
-function GetPhotoURLs(url){
-
-  var photo_urls= new Array();
+  var photo_urls=new Array();
 
   request({
       url: url,
       json: true
   }, function (error, response, body) {
 
-      if (!error && response.statusCode === 200) {
-          //console.log(body) // Print the json response
-          var inhale_jsonp=body;
-
+      if (!error && response.statusCode === 200)
+      {
           //clean and parse JSONp response from tumblr API
-          var blog_stream= inhale_jsonp.substring(22,inhale_jsonp.length-2);
+          var blog_stream= body.substring(22,body.length-2);
           blog_stream=JSON.parse(blog_stream);
-
-          //console.log(blog_stream['posts'][0]['photo-url-500']);
 
           for(var i=0;i<blog_stream.posts.length;i++)
           {
-            //console.log(i+": "+ blog_stream['posts'][i]['photo-url-500']);
             photo_urls[i]=blog_stream['posts'][i]['photo-url-500'];
-
+            //console.log(photo_urls[i]+"\n");
           }
+
+          res.json(photo_urls);
+
+          console.log("urls sent");
       }
       else
       {
           photo_urls[0]='none';
+          res.json(photo_urls);
+          console.log("nope!");
       }
   });
-
-  return photo_urls;
-
 }
 
 
@@ -59,7 +53,9 @@ app.get('/', function(request, response) {
 });
 
 app.get('/api/tumblr_test',function(request, response) {
-  response.json(GetPhotoURLs('http://rad0m1mg.tumblr.com/api/read/json'));
+
+  GetPhotoURLs('http://rad0m1mg.tumblr.com/api/read/json', response);
+  
 });
 
 
